d42b6d6dc46dda778f5cff17722f0978db2082f5
maxluna231 <mlunadelacruz300@gmail.com>
2024-12-09 21:08:50 -0600

    carpetas

---
+++ 01-Java-Basico/TiposDeDatos/src/tiposdedatos/TiposDeDatos.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiposdedatos;
+
+/**
+ *
+ * @author lunad
+ */
+public class TiposDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // tipo de dato + nombre de la variable
+      // Declaración de variables
+        int edad = 33;
+        String nombre = "Max";
+        char letra = 'a';
+        boolean bandera = true;
+        double estatura = 1.67;
+
+        // Imprimir mensaje en la consola
+        System.out.println("Hola " + nombre + ", tu edad es " + edad);
+    }
+    
+}
+
